8b8591802b60b99c15f112b6dd4918965f2073c1
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2021-03-03 08:20:58 -0500

    Just a test

--- models/UserBilling.js
+++ models/UserBilling.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 
 const UserBillingSchema = new mongoose.Schema({
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: true
-    },
-    order: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Order',
-        required: true
-    },
-    address: {
-        type: String,
-        trim: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+    // user: {
+    //   type: mongoose.Schema.ObjectId,
+    //   ref: 'User',
+    //   required: true
+    // },
+    // order: {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: 'Order',
+    //     required: true
+    // },
+    // address: {
+    //     type: String,
+    //     trim: true,
+    // },
+    // createdAt: {
+    //     type: Date,
+    //     default: Date.now
+    // }
 });
 
 module.exports = mongoose.model('UserBilling', UserBillingSchema);
