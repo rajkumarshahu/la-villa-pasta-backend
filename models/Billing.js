48aeac96b6293fe87ac1b900e363556713c1af35
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2021-03-11 17:29:44 -0500

    Implement geocoder using mapquest and node-goecoder

--- models/Billing.js
+++ models/Billing.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const geocoder = require('../utils/geocoder');
 
 const BillingSchema = new mongoose.Schema({
     user: {
@@ -12,17 +13,55 @@ const BillingSchema = new mongoose.Schema({
         required: true
     },
     address: {
-		streetNumber: { type: Number },
-		streetName: { type: String },
-		apartmentNumber: { type: Number },
-		city: { type: String },
-		province: { type: String },
-		postalCode: { type: String }
-	},
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+    // GeoJSON Point
+    type: {
+        type: String,
+        enum: ['Point']
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+    },
     createdAt: {
         type: Date,
         default: Date.now
     }
-});
+},
+{
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+}
+);
 
-module.exports = mongoose.model('Billing', BillingSchema);
+
+// Geocode & create location field
+BillingSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+      type: 'Point',
+      coordinates: [loc[0].longitude, loc[0].latitude],
+      formattedAddress: loc[0].formattedAddress,
+      streetName: loc[0].streetName,
+      city: loc[0].city,
+      province: loc[0].stateCode,
+      postalCode: loc[0].zipcode,
+      country: loc[0].countryCode
+    };
+
+    // Do not save address in DB
+    this.address = undefined;
+    next();
+  });
+
+  module.exports = mongoose.model('Billing', BillingSchema);
