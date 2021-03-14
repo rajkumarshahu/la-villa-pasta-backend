a67f3bf72f43cd10ecfaee5383eb870b8a5244ca
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2021-03-14 18:21:39 -0400

    Removed global authorization

--- routes/users.js
+++ routes/users.js
@@ -15,12 +15,12 @@ const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 router.use(protect);
-router.use(authorize('admin'));
+//router.use(authorize('admin'));
 
 router
   .route('/')
   .get(advancedResults(User), getUsers)
-  .post(createUser);
+  .post(protect, authorize('admin'), createUser);
 
 router
   .route('/:id')
