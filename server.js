753c708c1ced883739f0c86d378ad3b19593bc6f
Raj <rajkumarshahu@gmail.com>
2023-05-30 13:51:20 -0400

    api correction

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
-const colors = require('colors');
-const fileupload = require('express-fileupload');
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
-const cors = require('cors');
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+const cors = require("cors");
 
 // Load env variables
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
 
 // Route files
-const billings = require('./routes/billings');
-const items = require('./routes/items');
-const orders = require('./routes/orders');
-const orderDetails = require('./routes/orderDetails');
-const auth = require('./routes/auth');
-const users = require('./routes/users');
+const billings = require("./routes/billings");
+const items = require("./routes/items");
+const orders = require("./routes/orders");
+const orderDetails = require("./routes/orderDetails");
+const auth = require("./routes/auth");
+const users = require("./routes/users");
 
 const app = express();
 
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
+	res.header("Access-Control-Allow-Origin", "*");
 	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
 	);
-	if (req.method === 'OPTIONS') {
+	if (req.method === "OPTIONS") {
 		res.header(
-			'Access-Control-Allow-Headers',
-			'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept, Authorization"
 		);
-		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+		res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
 		return res.status(200).json({});
 	}
 	next();
@@ -53,7 +53,7 @@ app.use(cookieParser());
 // @desc    Logs request to console
 const logger = (req, res, next) => {
 	console.log(
-		`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
+		`${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
 	);
 	next();
 };
@@ -76,19 +76,18 @@ app.use(helmet());
 app.use(cors());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Mount routers
 
-app.use('/items', items);
-app.use('/billings', billings);
-app.use('/orders', orders);
-app.use('/orderDetails', orderDetails);
-app.use('/auth', auth);
-app.use('/users', users);
+app.use("/items", items);
+app.use("/billings", billings);
+app.use("/orders", orders);
+app.use("/orderDetails", orderDetails);
+app.use("/auth", auth);
+app.use("/users", users);
 
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -100,9 +99,9 @@ const server = app.listen(
 	)
 );
 
-module.exports = server
+module.exports = server;
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
 	console.log(`Error: ${err.message}`.red);
 	// Close server & exit process
 	server.close(() => process.exit(1));
